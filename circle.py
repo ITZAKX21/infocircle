e2d609d3bccdcef95dad13a086b099a95b0a47c8
Mohammad Alamin <93329400+ITZAKX21@users.noreply.github.com>
2022-06-29 17:26:34 +0600

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,10 @@
+import os, time, sys
+from time import sleep
+try:
+  import infocircle
+except:
+  os.system("pip3 install infocircle")
+def main ():
+  os.system("infocircle")
+if __name__=='__main__':
+  main ()
